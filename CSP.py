1a2f93cf6516d41315396c8fce86a68d34b2b1b5
BasCarpentero <61089858+BasCarpentero@users.noreply.github.com>
2020-11-03 18:40:07 +0100

    Add files via upload

--- CSP.py
+++ CSP.py
@@ -1,76 +1,69 @@
-# Common Spatial Pattern implementation in Python, used to build spatial filters for identifying task-related activity.
 import numpy as np
+from scipy.io import loadmat
 import scipy.linalg as la
 
-# CSP takes any number of arguments, but each argument must be a collection of trials associated with a task
-# That is, for N tasks, N arrays are passed to CSP each with dimensionality (# of trials of task N) x (feature vector)
-# Trials may be of any dimension, provided that each trial for each task has the same dimensionality,
-# otherwise there can be no spatial filtering since the trials cannot be compared
-def CSP(*tasks):
-	if len(tasks) < 2:
-		print "Must have at least 2 tasks for filtering."
-		return (None,) * len(tasks)
-	else:
-		filters = ()
-		# CSP algorithm
-		# For each task x, find the mean variances Rx and not_Rx, which will be used to compute spatial filter SFx
-		iterator = range(0,len(tasks))
-		for x in iterator:
-			# Find Rx
-			Rx = covarianceMatrix(tasks[x][0])
-			for t in range(1,len(tasks[x])):
-				Rx += covarianceMatrix(tasks[x][t])
-			Rx = Rx / len(tasks[x])
 
-			# Find not_Rx
-			count = 0
-			not_Rx = Rx * 0
-			for not_x in [element for element in iterator if element != x]:
-				for t in range(0,len(tasks[not_x])):
-					not_Rx += covarianceMatrix(tasks[not_x][t])
-					count += 1
-			not_Rx = not_Rx / count
+# A helper function to modify the given data, getting rid of Matlab's cell structures.
+def unwrap_cell_data(cell_data):
+    unwrapped_data = []
+    for x in cell_data:
+        unwrapped_data.append(x[0])
+    return unwrapped_data
 
-			# Find the spatial filter SFx
-			SFx = spatialFilter(Rx,not_Rx)
-			filters += (SFx,)
 
-			# Special case: only two tasks, no need to compute any more mean variances
-			if len(tasks) == 2:
-				filters += (spatialFilter(not_Rx,Rx),)
-				break
-		return filters
+# A helper function to classify given samples into their classes, returns a dictionary with keys 'class_x' and a list
+# of corresponding samples as values.
+def group_by_class(samples, sample_classes):
+    class_one = []
+    class_two = []
+    for x in sample_classes:
+        if x == 1:
+            class_one.append(np.transpose(samples[x]))
+        else:
+            class_two.append(np.transpose(samples[x]))
+    return {"class_one": class_one, "class_two": class_two}
 
-# covarianceMatrix takes a matrix A and returns the covariance matrix, scaled by the variance
-def covarianceMatrix(A):
-	Ca = np.dot(A,np.transpose(A))/np.trace(np.dot(A,np.transpose(A)))
-	return Ca
 
-# spatialFilter returns the spatial filter SFa for mean covariance matrices Ra and Rb
-def spatialFilter(Ra,Rb):
-	R = Ra + Rb
-	E,U = la.eig(R)
+# A helper function to compute covariance matrices
+def covariance_matrix(A):
+    return np.dot(A, np.transpose(A)) / np.trace(np.dot(A, np.trace(A)))
 
-	# CSP requires the eigenvalues E and eigenvector U be sorted in descending order
-	ord = np.argsort(E)
-	ord = ord[::-1] # argsort gives ascending order, flip to get descending
-	E = E[ord]
-	U = U[:,ord]
 
-	# Find the whitening transformation matrix
-	P = np.dot(np.sqrt(la.inv(np.diag(E))),np.transpose(U))
+# A helper function to compute composite covariance matrices given a list of classes.
+def spatial_covariance_matrices(grouped_data):
+    result = []
+    for group in grouped_data.values():
+        # Initialize an empty matrix.
+        spatial_covariance = covariance_matrix(group[0]) * 0
+        count = 0
+        for sample in group:
+            count += 1
+            spatial_covariance += covariance_matrix(sample)
+        result.append(spatial_covariance / count)
+    return result
 
-	# The mean covariance matrices may now be transformed
-	Sa = np.dot(P,np.dot(Ra,np.transpose(P)))
-	Sb = np.dot(P,np.dot(Rb,np.transpose(P)))
 
-	# Find and sort the generalized eigenvalues and eigenvector
-	E1,U1 = la.eig(Sa,Sb)
-	ord1 = np.argsort(E1)
-	ord1 = ord1[::-1]
-	E1 = E1[ord1]
-	U1 = U1[:,ord1]
+# Computes a spatial filter
+def CSP(class_covariances):
+    # Solve the generalized eigenvalue problem resulting in eigenvalues and corresponding eigenvectors and
+    # sort them in descending order.
+    eigenvalues, eigenvectors = la.eigh(class_covariances[0], class_covariances[1])
+    ascending_eigenvalues = np.argsort(eigenvalues)
+    descending_eigenvalues = ascending_eigenvalues[::-1]
+    eigenvalues = eigenvalues[descending_eigenvalues]
+    eigenvectors = eigenvectors[:, descending_eigenvalues]
+    print(eigenvalues)
+    print(eigenvectors.shape)
 
-	# The projection matrix (the spatial filter) may now be obtained
-	SFa = np.dot(np.transpose(U1),P)
-	return SFa.astype(np.float32)
+
+if __name__ == "__main__":
+
+    data = loadmat('/Users/ogppr/Downloads/dataSubject8')
+    wrapped_attended_ear = np.array(data.get('attendedEar'))
+    attended_ear = unwrap_cell_data(wrapped_attended_ear)
+    wrapped_EEG_data = np.array(data.get('eegTrials'))
+    EEG_data = unwrap_cell_data(wrapped_EEG_data)
+
+    grouped_data = group_by_class(EEG_data, attended_ear)
+    class_covariances = spatial_covariance_matrices(grouped_data)
+    CSP(class_covariances)
