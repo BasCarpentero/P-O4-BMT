eec56da1198cb0df89f258b42bd80cc5825c51c8
Nele Eeckman <nele.eeckman@student.kuleuven.be>
2020-11-15 17:26:13 +0100

    test waarden

--- main.py
+++ main.py
@@ -76,7 +76,8 @@ def classify(D):
 
 
 if __name__ == "__main__":
-    data = loadmat('/Users/ogppr/Documents/dataSubject8.mat')
+    #data = loadmat('/Users/ogppr/Documents/dataSubject8.mat')
+    data = loadmat('dataSubject8.mat')
     wrapped_attended_ear = np.array(data.get('attendedEar'))
     attended_ear = CSP.unwrap_cell_data(wrapped_attended_ear)
     attended_ear_1 = CSP.unwrap_cell_data(wrapped_attended_ear)[:36]
@@ -95,17 +96,74 @@ if __name__ == "__main__":
     W = CSP.CSP(class_covariances)
 
     wrapped_x = np.array(data.get('eegTrials'))
+    #geval 1: test data 12-48
+    begin, end = 12, 48
     f = calculate_f(begin, end, W, wrapped_x)
     inv_cov_mat = get_covariance_matrix(f)
-    f_in_classes = group_by_class(f, attended_ear)
+    f_in_classes = group_by_class(f, attended_ear[12:48])
     mean1 = calculate_mean(np.array(f_in_classes[0]))
     mean2 = calculate_mean(np.array(f_in_classes[1]))
     v_t, b = calculate_vt_b(inv_cov_mat, mean1, mean2)
-    attended_ear2 = CSP.unwrap_cell_data(wrapped_attended_ear)[36:]
+    attended_ear2 = CSP.unwrap_cell_data(wrapped_attended_ear)[:12]
+    f = calculate_f(0, 12, W, wrapped_x)
+    count = 0
+    for i in range(12):
+        D = calculate_D(v_t, b, f[i])
+        if attended_ear2[i] != classify(D):
+            count += 1
+    print("Count:", count, (100 - (count*100/12)), "% juist")  # Aantal verkeerd voorspelde minuten (veel te hoog!!)
+
+    # geval 2: verificatie 12-24-> 12-23
+    f = []
+    print(np.shape(calculate_f(0, 12, W, wrapped_x)))
+    f.append(calculate_f(0, 12, W, wrapped_x))
+    f.append(calculate_f(24, 48, W, wrapped_x))
+    print(np.shape(f))
+    inv_cov_mat = get_covariance_matrix(f)
+    attended_ear2 = np.array([attended_ear[0:12],attended_ear[24:48]])
+    f_in_classes = group_by_class(f, attended_ear2)
+    mean1 = calculate_mean(np.array(f_in_classes[0]))
+    mean2 = calculate_mean(np.array(f_in_classes[1]))
+    v_t, b = calculate_vt_b(inv_cov_mat, mean1, mean2)
+    attended_ear2 = CSP.unwrap_cell_data(wrapped_attended_ear)[12:24]
+    f = calculate_f(12, 24, W, wrapped_x)
+    count = 0
+    for i in range(12):
+        D = calculate_D(v_t, b, f[i])
+        if attended_ear2[i] != classify(D):
+            count += 1
+    print("Count:", count, (100 - (count * 100 / 12)), "% juist")  # Aantal verkeerd voorspelde minuten (veel te hoog!!)
+
+    # geval 3: verify 24-36
+    f = [calculate_f(0, 24, W, wrapped_x), calculate_f(36, 48, W, wrapped_x)]
+    f = np.array(f)
+    inv_cov_mat = get_covariance_matrix(f)
+    attended_ear2 = np.array([attended_ear[0:12], attended_ear[24:48]])
+    f_in_classes = group_by_class(f, attended_ear2)
+    mean1 = calculate_mean(np.array(f_in_classes[0]))
+    mean2 = calculate_mean(np.array(f_in_classes[1]))
+    v_t, b = calculate_vt_b(inv_cov_mat, mean1, mean2)
+    attended_ear2 = CSP.unwrap_cell_data(wrapped_attended_ear)[24:36]
+    f = calculate_f(24, 36, W, wrapped_x)
+    count = 0
+    for i in range(12):
+        D = calculate_D(v_t, b, f[i])
+        if attended_ear2[i] != classify(D):
+            count += 1
+    print("Count:", count, (100 - (count * 100 / 12)), "% juist")  # Aantal verkeerd voorspelde minuten (veel te hoog!!)
+
+    # geval 4: verificatie 36-48
+    f = calculate_f(0, 36, W, wrapped_x)
+    inv_cov_mat = get_covariance_matrix(f)
+    f_in_classes = group_by_class(f, attended_ear[0:36])
+    mean1 = calculate_mean(np.array(f_in_classes[0]))
+    mean2 = calculate_mean(np.array(f_in_classes[1]))
+    v_t, b = calculate_vt_b(inv_cov_mat, mean1, mean2)
+    attended_ear2 = CSP.unwrap_cell_data(wrapped_attended_ear)[36:48]
     f = calculate_f(36, 48, W, wrapped_x)
     count = 0
     for i in range(12):
         D = calculate_D(v_t, b, f[i])
         if attended_ear2[i] != classify(D):
             count += 1
-    print("Count:", count)  # Aantal verkeerd voorspelde minuten (veel te hoog!!)
+    print("Count:", count, (100 - (count * 100 / 12)), "% juist")  # Aantal verkeerd voorspelde minuten (veel te hoog!!)
