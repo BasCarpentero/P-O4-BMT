b1ac2497e1e8c2471c5f9dd1cefc32cc52c08044
SofieMareels <56159771+SofieMareels@users.noreply.github.com>
2020-12-07 14:39:32 +0100

    Few changes
    Put our code back in the main folder

---
+++ AuxiliaryFunctions.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+# Modifies the given data, getting rid of Matlab's cell structures.
+def unwrap_cell_data(cell_data):
+    unwrapped_data = []
+    for x in cell_data:
+        unwrapped_data.append(x[0])
+    return unwrapped_data
+
+
+# A helper function to compute covariance matrices
+def covariance_matrix(A):
+    return np.cov(A)
+
+
+# A helper function to classify given samples into their classes, returns a dictionary with keys 'class_x' and a list
+# of corresponding samples as values.
+def group_by_class(samples, sample_classes):
+    class_one = []
+    class_two = []
+    for i in range(len(samples)):
+        if sample_classes[i] == 1:
+            class_one.append(np.transpose(samples[i]))
+        else:
+            class_two.append(np.transpose(samples[i]))
+    return np.array([class_one, class_two])
---
+++ LDA.py
@@ -0,0 +1,76 @@
+"""
+This script contains the code for feature vector calculation and linear discriminant analysis.
+"""
+from scipy.fftpack import fft, fftfreq
+import matplotlib.pyplot as plt
+from scipy.signal import freqz
+from scipy.io import loadmat
+import scipy.fftpack
+import scipy
+
+
+import numpy as np
+
+import AuxiliaryFunctions
+import Filter
+import CSP
+import LDA
+# class 1: LEFT
+# class 2: RIGHT
+
+# Returns the log-energy vector computed with T samples given the CSP-filtered data y.
+def logenergy(y):
+    outputEnergyVector = np.zeros(len(y))
+    for i in range(len(y)):
+        outputEnergyVector[i] = sum(j**2 for j in y[i])
+    return np.log(outputEnergyVector)
+
+
+def calculate_f(testMinutes, W, x):
+    f = []
+    for i in testMinutes:
+        # x is a C x A matrix with C = #channels and A = #time instances
+        # W is a C x K matrix with C = #channels and K = # spatial filters
+        # T is the decisionwindow
+        y = np.dot(np.transpose(W), np.transpose(x[i]))
+        f.append(logenergy(y))
+    return np.array(f)
+
+
+def calculate_mean(data):
+    data = np.transpose(data)
+    means = []
+    for i in range(np.shape(data)[0]):
+        means.append(np.mean(data[i]))
+    return means
+
+
+def calculate_vt_b(inv_cov_mat, m1, m2):
+    diff_mean = np.subtract(m2, m1)
+    sum_mean = np.add(m1,m2)
+    v = np.dot(inv_cov_mat, diff_mean)
+    v_t = v.transpose()
+    b = -0.5 * np.dot(v_t, sum_mean)
+    return v_t, b
+
+
+def calculate_D(v_t, f, b):
+    D = []
+    for i in range(len(f)):
+        D.append(np.dot(v_t, f[i]) + b)
+    return np.array(D)
+
+def classify(D):
+    classification = []
+    for value in D:
+        if value < 0:
+            classification.append(1)
+        else:
+            classification.append(2)
+    return np.array(classification)
+#
+# def classify(v_t, b, f):
+#     if np.dot(v_t, f) + b < 0:
+#         return 1
+#     else:
+#         return 2
